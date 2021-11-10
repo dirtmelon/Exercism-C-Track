d74de94801945d11e5b794672c75430826b87338
liguofeng <liguofeng@yy.com>
2021-11-10 12:57:23 +0800

    hamming

--- hamming/hamming.c
+++ hamming/hamming.c
@@ -3,11 +3,14 @@
 int compute(const char *lhs, const char *rhs)
 {
     int i, distance = 0;
-    for (i = 0; lhs[i] != '\0'; i++)
+    for (i = 0; lhs[i] != '\0' && rhs[i] != '\0'; i++)
     {
       if (lhs[i] != rhs[i]) {
         distance += 1;
       }
+    }
+    if (lhs[i] != '\0' || rhs[i] != '\0') {
+      return -1;
     }
     return distance;
 }
